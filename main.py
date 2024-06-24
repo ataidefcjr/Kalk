fe1d4a6fa91a4727a5f71f5ecdba7268abd3c46d
ataide <juninho.ataide@gmail.com>
2024-06-24 11:25:39 -0300

    Ajustado interface para preencher automaticamente

--- main.py
+++ main.py
@@ -1,78 +1,84 @@
-from tkinter import Tk, Frame, StringVar, Label, FLAT, RIGHT, Button
-from tkinter import *
+import tkinter as tk    
 
 valor_atual = ''
 lista_teclas=['0','1','2','3','4','5','6','7','8','9','/','*','-','+','.','Return', 'KP_Enter','=', 'BackSpace', 'Escape']
-janela = Tk()
+
+## Criação da janela principal ##
+janela = tk.Tk()
 janela.title("Kalk")
-
-janela.geometry("350x430")
+janela.geometry("300x400")
 janela.config(bg='#1e1e1e')
-janela.resizable(False, False)
 
-#Frames
-frame_tela = Frame(janela, width = 350, height = 70, bg="#292929", borderwidth='10')
-frame_tela.grid(row=0, column=0)
-frame_botoes = Frame(janela, width = 350, height = 360, bg="#454545", borderwidth='10')
-frame_botoes.grid(row=1, column=0)
 
-#Label
-visor = StringVar()
-tamanho = '25'
+## Frames ##
+frame_tela = tk.Frame(janela, bg="#292929", borderwidth='1')
+frame_tela.grid(row=0, column=0, sticky="nsew")
+
+frame_botoes = tk.Frame(janela, bg="#454545", borderwidth='1')
+frame_botoes.grid(row=1, column=0, sticky="nsew")
+
+## Configuração dos grids ##
+janela.grid_rowconfigure(0, weight=1)
+janela.grid_rowconfigure(1, weight=5)
+janela.grid_columnconfigure(0, weight=1)
+frame_tela.grid_columnconfigure(0, weight=1)
+frame_tela.grid_rowconfigure(0, weight=1)
+frame_botoes.grid_columnconfigure((0, 1, 2, 3), weight=1)
+frame_botoes.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+
+## Label ##
+visor = tk.StringVar()
+tamanho = '22'
 fonte = f"times {str(tamanho)}"
+tela_label = tk.Label(frame_tela, textvariable=visor, relief=tk.FLAT, anchor='e', justify=tk.RIGHT, font=fonte, bg="#292929", fg="#fafafa")
+tela_label.grid(row=0, column=0, sticky="nsew", padx=6, pady=2)
 
-tela_label = Label(frame_tela, textvariable=visor, width = 15, height = 1, relief=FLAT, anchor='e', justify=RIGHT, font = fonte, bg="#292929", fg="#fafafa")
-tela_label.place(x=6,y=8)
-
-## Botoes
-largura = 4
-altura = 2
+#### Botões ####
 cor1 = "#c0c0c0"
 cor2 = "#FFA500"
 font = ("calibri 14 bold")
-x = 84
-y = 70
-#Primeira Fileira
-btc = Button(frame_botoes, command = lambda: entrada('clear'), text="C", width=11, height=altura, bg=cor1, font=font)
-btc.place(x=0,y=0)
-btp = Button(frame_botoes, command = lambda: entrada('%'), text="%", width=largura, height=altura, bg=cor2, font =font)
-btp.place(x=x*2,y=0)
-btb = Button(frame_botoes, command = lambda: entrada('/'), text="/", width=largura, height=altura, bg=cor2, font=font)
-btb.place(x=x*3,y=0)
-#Segunda Fileira
-bt7 = Button(frame_botoes, command = lambda: entrada('7'), text="7", width=largura, height=altura, bg=cor1, font=font)
-bt7.place(x=0,y=y)
-bt8 = Button(frame_botoes, command = lambda: entrada('8'), text="8", width=largura, height=altura, bg=cor1, font=font)
-bt8.place(x=x,y=y)
-bt9 = Button(frame_botoes, command = lambda: entrada('9'), text="9", width=largura, height=altura, bg=cor1, font=font)
-bt9.place(x=x*2,y=y)
-bta = Button(frame_botoes, command = lambda: entrada('*'), text="*", width=largura, height=altura, bg=cor2, font=font)
-bta.place(x=x*3,y=y)
-#Terceita Fileira
-bt4 = Button(frame_botoes, command = lambda: entrada('4'), text="4", width=largura, height=altura, bg=cor1, font=font)
-bt4.place(x=0,y=y*2)
-bt5 = Button(frame_botoes, command = lambda: entrada('5'), text="5", width=largura, height=altura, bg=cor1, font=font)
-bt5.place(x=x,y=y*2)
-bt6 = Button(frame_botoes, command = lambda: entrada('6'), text="6", width=largura, height=altura, bg=cor1, font=font)
-bt6.place(x=x*2,y=y*2)
-btplus = Button(frame_botoes, command = lambda: entrada('+'), text="+", width=largura, height=altura, bg=cor2, font=font)
-btplus.place(x=x*3,y=y*2)
-#Quarta Fileira
-bt1 = Button(frame_botoes, command = lambda: entrada('1'), text="1", width=largura, height=altura, bg=cor1, font=font)
-bt1.place(x=0,y=y*3)
-bt2 = Button(frame_botoes, command = lambda: entrada('2'), text="2", width=largura, height=altura, bg=cor1, font=font)
-bt2.place(x=x,y=y*3)
-bt3 = Button(frame_botoes, command = lambda: entrada('3'), text="3", width=largura, height=altura, bg=cor1, font=font)
-bt3.place(x=x*2,y=y*3)
-btm = Button(frame_botoes, command = lambda: entrada('-'), text="-", width=largura, height=altura, bg=cor2, font=font)
-btm.place(x=x*3,y=y*3)
-#Quinta Fileira
-bt0 = Button(frame_botoes, command = lambda: entrada('0'), text="0", width=11, height=altura, bg=cor1, font=font)
-bt0.place(x=0,y=y*4)
-btd = Button(frame_botoes, command = lambda: entrada('.'), text=".", width=largura, height=altura, bg=cor1, font=font)
-btd.place(x=x*2,y=y*4)
-bte = Button(frame_botoes, command = lambda: calcular(), text="=", width=largura, height=altura, bg=cor2, font=font)
-bte.place(x=x*3,y=y*4)
+
+# Primeira Fileira
+btc = tk.Button(frame_botoes, command=lambda: entrada('clear'), text="C", bg=cor1, font=font)
+btc.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=3, pady=3)
+btp = tk.Button(frame_botoes, command=lambda: entrada('%'), text="%", bg=cor2, font=font)
+btp.grid(row=0, column=2, sticky="nsew", padx=3, pady=3)
+btb = tk.Button(frame_botoes, command=lambda: entrada('/'), text="/", bg=cor2, font=font)
+btb.grid(row=0, column=3, sticky="nsew", padx=3, pady=3)
+# Segunda Fileira
+bt7 = tk.Button(frame_botoes, command = lambda: entrada('7'), text="7", bg=cor1, font=font)
+bt7.grid(row=1,column=0, sticky="nsew", padx=3, pady=3)
+bt8 = tk.Button(frame_botoes, command = lambda: entrada('8'), text="8", bg=cor1, font=font)
+bt8.grid(row=1,column=1, sticky="nsew", padx=3, pady=3)
+bt9 = tk.Button(frame_botoes, command = lambda: entrada('9'), text="9", bg=cor1, font=font)
+bt9.grid(row=1,column=2, sticky="nsew", padx=3, pady=3)
+bta = tk.Button(frame_botoes, command = lambda: entrada('*'), text="*", bg=cor2, font=font)
+bta.grid(row=1,column=3, sticky="nsew", padx=3, pady=3)
+# Terceita Fileira
+bt4 = tk.Button(frame_botoes, command = lambda: entrada('4'), text="4", bg=cor1, font=font)
+bt4.grid(row=2,column=0, sticky="nsew", padx=3, pady=3)
+bt5 = tk.Button(frame_botoes, command = lambda: entrada('5'), text="5", bg=cor1, font=font)
+bt5.grid(row=2,column=1, sticky="nsew", padx=3, pady=3)
+bt6 = tk.Button(frame_botoes, command = lambda: entrada('6'), text="6", bg=cor1, font=font)
+bt6.grid(row=2,column=2, sticky="nsew", padx=3, pady=3)
+btplus = tk.Button(frame_botoes, command = lambda: entrada('+'), text="+", bg=cor2, font=font)
+btplus.grid(row=2,column=3, sticky="nsew", padx=3, pady=3)
+# Quarta Fileira
+bt1 = tk.Button(frame_botoes, command = lambda: entrada('1'), text="1", bg=cor1, font=font)
+bt1.grid(row=3,column=0, sticky="nsew", padx=3, pady=3)
+bt2 = tk.Button(frame_botoes, command = lambda: entrada('2'), text="2", bg=cor1, font=font)
+bt2.grid(row=3,column=1, sticky="nsew", padx=3, pady=3)
+bt3 = tk.Button(frame_botoes, command = lambda: entrada('3'), text="3", bg=cor1, font=font)
+bt3.grid(row=3,column=2, sticky="nsew", padx=3, pady=3)
+btm = tk.Button(frame_botoes, command = lambda: entrada('-'), text="-", bg=cor2, font=font)
+btm.grid(row=3,column=3, sticky="nsew", padx=3, pady=3)
+# Quinta Fileira
+bt0 = tk.Button(frame_botoes, command = lambda: entrada('0'), text="0", bg=cor1, font=font)
+bt0.grid(row=4,column=0, columnspan=2, sticky="nsew", padx=3, pady=3)
+btd = tk.Button(frame_botoes, command = lambda: entrada('.'), text=".", bg=cor1, font=font)
+btd.grid(row=4,column=2, sticky="nsew", padx=3, pady=3)
+bte = tk.Button(frame_botoes, command = lambda: calcular(), text="=", bg=cor2, font=font)
+bte.grid(row=4,column=3, sticky="nsew", padx=3, pady=3)
 
 #Recebe a entrada e apresenta na tela
 def entrada(character):
@@ -81,6 +87,9 @@ def entrada(character):
     if valor_atual == 'Erro' or valor_atual == '0' or valor_atual == 'clear' or valor_atual[-1:] == '~':
         valor_atual = ''
         if character == 'clear':
+            valor_atual == ''
+            visor.set(valor_atual)
+        elif character == 'backspace':
             valor_atual == ''
             visor.set(valor_atual)
         else:
@@ -112,9 +121,6 @@ def calcular():
                 resultado = int(resultado)
             #Transforma em string
             str_resultado = str(resultado)
-            #Se o resultado for maior que 14 caracteres entao trunca em 13 e apresenta ...
-            if len(str_resultado) > 14:
-                str_resultado = str_resultado[:14] +'~'
             #Define o valor atual como o valor do resultado e seta o visor com o valor
             valor_atual = str_resultado
             visor.set(valor_atual)
